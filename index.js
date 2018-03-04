8aa92f21c865b84be31faa750a426932906ad95a
victoryk13 <30733872+victoryk13@users.noreply.github.com>
2018-03-04 12:03:10 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var $tbody = document.querySelector("tbody");
+var $tbody = document.querySelector("#myTableBody");
 var $fetchBtn = document.querySelector("#get");
 var $dateInput = document.querySelector("#date");
 var $cityInput = document.querySelector("#city");
@@ -6,6 +6,7 @@ var $stateInput = document.querySelector("#state");
 var $countryInput = document.querySelector("#country");
 var $shapeInput = document.querySelector("#shape");
 var $searchBtn = document.querySelector("#search");
+var $pager = document.querySelector("#myPager");
 var count = 0;
 
 $fetchBtn.addEventListener("click", renderTableRow)
@@ -13,9 +14,112 @@ $searchBtn.addEventListener("click", handleSearchButtonClick);
 
 var filteredSightings = dataSet;
 
+$.fn.pageMe = function(opts){
+  var $this = this,
+      defaults = {
+          perPage: 7,
+          showPrevNext: false,
+          hidePageNumbers: false
+      },
+      settings = $.extend(defaults, opts);
+
+  var listElement = $this;
+  var perPage = settings.perPage; 
+  var children = listElement.children();
+  var pager = $('.pager');
+
+  if (typeof settings.childSelector!="undefined") {
+      children = listElement.find(settings.childSelector);
+  }
+
+  if (typeof settings.pagerSelector!="undefined") {
+      pager = $(settings.pagerSelector);
+  }
+
+  var numItems = children.size();
+  var numPages = Math.ceil(numItems/perPage);
+
+  pager.data("curr",0);
+
+  if (settings.showPrevNext){
+      $('<li><a href="#" class="prev_link">«</a></li>').appendTo(pager);
+  }
+
+  var curr = 0;
+  while(numPages > curr && (settings.hidePageNumbers==false)){
+      $('<li><a href="#" class="page_link">'+(curr+1)+'</a></li>').appendTo(pager);
+      curr++;
+  }
+
+  if (settings.showPrevNext){
+      $('<li><a href="#" class="next_link">»</a></li>').appendTo(pager);
+  }
+
+  pager.find('.page_link:first').addClass('active');
+  pager.find('.prev_link').hide();
+  if (numPages<=1) {
+      pager.find('.next_link').hide();
+  }
+  pager.children().eq(1).addClass("active");
+
+  children.hide();
+  children.slice(0, perPage).show();
+
+  pager.find('li .page_link').click(function(){
+      var clickedPage = $(this).html().valueOf()-1;
+      goTo(clickedPage,perPage);
+      return false;
+  });
+  pager.find('li .prev_link').click(function(){
+      previous();
+      return false;
+  });
+  pager.find('li .next_link').click(function(){
+      next();
+      return false;
+  });
+
+  function previous(){
+      var goToPage = parseInt(pager.data("curr")) - 1;
+      goTo(goToPage);
+  }
+
+  function next(){
+      goToPage = parseInt(pager.data("curr")) + 1;
+      goTo(goToPage);
+  }
+
+  function goTo(page){
+      var startAt = page * perPage,
+          endOn = startAt + perPage;
+
+      children.css('display','none').slice(startAt, endOn).show();
+
+      if (page>=1) {
+          pager.find('.prev_link').show();
+      }
+      else {
+          pager.find('.prev_link').hide();
+      }
+
+      if (page<(numPages-1)) {
+          pager.find('.next_link').show();
+      }
+      else {
+          pager.find('.next_link').hide();
+      }
+
+      pager.data("curr",page);
+      pager.children().removeClass("active");
+      pager.children().eq(page+1).addClass("active");
+
+  }
+};
+
 function renderTableRow() {
   if ($dateInput.value != '' || $cityInput.value != '' || $stateInput.value != '' || $countryInput.value != '' || $shapeInput.value != '') {
     $tbody.innerHTML = "";
+    $pager.innerHTML = "";
     document.getElementById("date").value = "";
     document.getElementById("city").value = "";
     document.getElementById("state").value = "";
@@ -38,6 +142,7 @@ function renderTableRow() {
 
 function renderTable() {
   $tbody.innerHTML = "";
+  $pager.innerHTML = "";
   for (var i = 0; i < filteredSightings.length; i++) {
 
     var sighting = filteredSightings[i];
@@ -51,6 +156,7 @@ function renderTable() {
       $cell.innerText = sighting[field];
     }
   }
+  $('#myTableBody').pageMe({pagerSelector:'#myPager',showPrevNext:true,hidePageNumbers:false,perPage:50});
 }
 
 
